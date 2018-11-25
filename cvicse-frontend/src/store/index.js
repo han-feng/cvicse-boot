a7d2b71ee5f15c0b621b02af0b60ef65cdf81ae9
han_feng <han_feng@foxmail.com>
2018-11-25 14:44:37 +0800

    初步使用vuex-along保存许可到session

--- cvicse-frontend/src/store/index.js
+++ cvicse-frontend/src/store/index.js
@@ -1,12 +1,20 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import vuexAlong from 'vuex-along'
 
 import d2admin from './modules/d2admin'
+import setting from '@/setting.js'
 
 Vue.use(Vuex)
+
+vuexAlong.setKey(`${setting.releases.name}-${setting.releases.version}`)
+vuexAlong.onlySession(true)
+// vuexAlong.watch(['d2admin'], true)
+// vuexAlong.watchSession(['session'], true)
 
 export default new Vuex.Store({
   modules: {
     d2admin
-  }
+  },
+  plugins: [vuexAlong]
 })
