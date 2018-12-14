3b6297ae851e392295538d188f97d61f2eafab29
han_feng <han_feng@foxmail.com>
2018-12-14 22:13:19 +0800

    完善菜单设置

--- cvicse-frontend/src/menu/index.js
+++ cvicse-frontend/src/menu/index.js
@@ -1,8 +1,8 @@
 // 菜单数据
 export default [
   {
-    title: '技术验证',
-    icon: 'desktop',
+    title: '演示',
+    icon: 'flask',
     children: [
       // { path: '/index', title: '首页', icon: 'home' },
       { path: '/demo/auth', title: '权限演示', icon: 'universal-access' },
@@ -10,16 +10,16 @@ export default [
         title: '参数路由',
         icon: 'sitemap',
         children: [
-          { path: '/demo/params/1', title: '编号 1' },
-          { path: '/demo/params/2', title: '编号 2' },
-          { path: '/demo/params/3', title: '编号 3' }
+          { path: '/demo/params/1', title: '编号 1', icon: 'hashtag' },
+          { path: '/demo/params/2', title: '编号 2', icon: 'hashtag' },
+          { path: '/demo/params/3', title: '编号 3', icon: 'hashtag' }
         ]
       }
     ]
   },
   {
     title: '设置',
-    icon: 'cog',
+    icon: 'gear',
     children: [
       { path: '/demo/page1', title: '页面 1' },
       { path: '/demo/page2', title: '页面 2' },
