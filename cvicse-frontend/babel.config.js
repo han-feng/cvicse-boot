6dd9d767e19574d8a573d7759f1df097ed1704ae
han_feng <han_feng@foxmail.com>
2018-12-26 22:09:29 +0800

    修复构建错误

--- cvicse-frontend/babel.config.js
+++ cvicse-frontend/babel.config.js
@@ -1,15 +1,21 @@
+
+const plugins = [
+  [
+    'component',
+    {
+      libraryName: 'element-ui',
+      styleLibraryName: 'theme-chalk'
+    }
+  ]
+]
+
+if (process.env.NODE_ENV === 'test') {
+  plugins.push('require-context-hook')
+}
+
 module.exports = {
   presets: [
     '@vue/app'
   ],
-  plugins: [
-    [
-      'component',
-      {
-        libraryName: 'element-ui',
-        styleLibraryName: 'theme-chalk'
-      }
-    ],
-    'require-context-hook'
-  ]
+  plugins
 }
