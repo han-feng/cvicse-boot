c4ea7d3e15d12048e04c506af7378a9239b2ba44
han_feng <han_feng@foxmail.com>
2018-12-28 14:47:35 +0800

    完善单元测试结构

--- cvicse-frontend/babel.config.js
+++ cvicse-frontend/babel.config.js
@@ -1,4 +1,4 @@
-
+// 使用 component 插件实现 Element UI 按需加载
 const plugins = [
   [
     'component',
@@ -8,7 +8,7 @@ const plugins = [
     }
   ]
 ]
-
+// 使用 require-context-hook 插件解决 jest 不能解析 require.context 的问题，仅限 test 模式使用
 if (process.env.NODE_ENV === 'test') {
   plugins.push('require-context-hook')
 }
