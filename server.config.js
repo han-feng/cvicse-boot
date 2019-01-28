b1f0ed52e2ce5961292aaa1e0886647c22dd646c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-28 09:05:45 +0000

    Deploy han-feng/cvicse-boot to github.com/han-feng/cvicse-boot.git:gh-pages

--- server.config.js
+++ server.config.js
@@ -2,13 +2,9 @@
 // 可以在部署或运行时改变参数，从而影响前端行为
 
 // eslint-disable-next-line no-unused-vars
-var serverConfig;
-
-(function () {
-  var baseUrl = '/api/'
-  serverConfig = {
-    'service.baseUrl': baseUrl,
-    'service.timeout': 5000, // 请求超时时间
-    'service.login.url': 'login'
-  }
-})()
+var serverConfig = {
+  'service.baseUrl': '/api/',
+  'service.timeout': 5000, // 请求超时时间
+  // login service
+  'service.login.url': 'login'
+}
